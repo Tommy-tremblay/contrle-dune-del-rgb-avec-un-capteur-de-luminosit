f95baf85908568d194b96162fd8e979ee3082282
Tommy-tremblay <116100321+Tommy-tremblay@users.noreply.github.com>
2022-11-03 14:05:56 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,16 @@ function bleu () {
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P8, 1)
 }
+function blanc () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function mauve () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
 function rouge () {
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 0)
@@ -16,10 +26,14 @@ function vert () {
 }
 basic.forever(function () {
     intensité = pins.analogReadPin(AnalogPin.P0)
-    if (intensité >= 500) {
+    if (intensité >= 700) {
         vert()
-    } else if (intensité < 500 && intensité >= 200) {
+    } else if (intensité < 750 && intensité > 350) {
         bleu()
+    } else if (intensité < 350 && intensité > 300) {
+        mauve()
+    } else if (intensité < 300 && intensité > 250) {
+        blanc()
     } else {
         rouge()
     }
